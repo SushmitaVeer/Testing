640a35040ea58c73b38a2b6a746caa08ebd7a273
Sushmita Veer <143728331+SushmitaVeer@users.noreply.github.com>
2023-09-04 13:25:34 +0530

    Add files via upload

---
+++ TestProject/src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		int a = 40;
+		int b = 35;
+		int total = a+b;
+		System.out.println("Addition is : "+total);
+	}
+
+}
